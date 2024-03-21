2f4daf0f2112dd2565d875ae5c111dd8713c8f9f
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-21 08:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let selectedSquare;
+const unrankedDropZone = document.getElementById("unranked-drop-zone");
+
+//--ITEMS
+document.querySelectorAll(".item").forEach((square) => {
+  square.addEventListener("dragstart", (e) => (selectedSquare = e.target));
+  square.addEventListener("dblclick", function () {
+    if (this.parentElement != unrankedDropZone)
+      unrankedDropZone.appendChild(this);
+  });
+});
+
+//--ZONES
+document.querySelectorAll(".drop-zone").forEach((dropZone) => {
+  dropZone.addEventListener("dragover", (e) => e.preventDefault());
+  dropZone.addEventListener("drop", function () {
+    if (selectedSquare.parentElement === this) return;
+    this.appendChild(selectedSquare);
+  });
+});
